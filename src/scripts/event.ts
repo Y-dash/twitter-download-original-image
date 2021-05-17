6b3dcc49761f9de02a27e075595791b73fa52d08
Y-dash <y-dash@y-dash.net>
2021-05-17 18:03:16 +0900

    Update: Twitterの最新仕様に合わせて整理。他拡張機能と競合するらしいのでついで

--- src/scripts/event.ts
+++ src/scripts/event.ts
@@ -1,4 +1,4 @@
-const CONTEXT_ID: string = 'download-original-image';
+const CONTEXT_ID: string = 'twitter-download-original-image.y-dash.net';
 let isCreatedContextMenu: boolean = false;
 
 chrome.runtime.onInstalled.addListener(init);
@@ -6,7 +6,7 @@ chrome.runtime.onStartup.addListener(init);
 
 chrome.contextMenus.onClicked.addListener((info) => {
 	if (info.srcUrl && (info.menuItemId === CONTEXT_ID)) {
-		download(info.srcUrl);
+		download(new URL(info.srcUrl));
 	}}
 );
 
@@ -25,56 +25,30 @@ function init(): void {
 	isCreatedContextMenu = true;
 }
 
-function download(src: string): void {
-	let sourceUri: string = replaceUrlWithBiggest(src);
+function download(src: URL): void {
+	let sourceUri: URL = replaceUrlWithBiggest(src);
 	let fileName: string = makeFileNameFromUrl(sourceUri);
 
 	chrome.downloads.download({
-		url: sourceUri,
+		url: sourceUri.toString(),
 		filename: fileName
 	});
 }
 
-function replaceUrlWithBiggest(src: string): string {
-	if (src.indexOf(':orig') !== -1) {
+function replaceUrlWithBiggest(src: URL): URL {
+	let srcString: string = src.toString();
+	
+	if (srcString.indexOf('=orig') !== -1) {
 		return src;
 	}
 
-	// 新UI以降のURL
-	if (src.indexOf('?format=') !== -1) {
-		src = src.replace('?format=', '.').split('&name=')[0] += ':orig';
-	}
-	// 旧UI時のURL
-	else {
-		let srcSplitColons: string[] = src.split(':');
-		src = `${srcSplitColons[0]}:${srcSplitColons[1]}:orig`;
-	}
+	srcString = srcString.replace(/name=.*$/, 'name=orig');
 
-	if (!getSrcExist(src)) {
-		src = src.replace(':orig', ':large');
-
-		if (!getSrcExist(src)) {
-			src = src.replace(':large', '');
-		}
-	}
-
+	src = new URL(srcString);
 	return src;
 }
 
-function getSrcExist(src: string): boolean {
-	let xmlHttpRequset: XMLHttpRequest = new XMLHttpRequest();
-	xmlHttpRequset.open('GET', src.toString(), false);
-	xmlHttpRequset.send();
-
-	if ((xmlHttpRequset.status === 200) || (xmlHttpRequset.status === 500)) {
-		return true;
-	}
-	return false;
-}
-
-function makeFileNameFromUrl(url: string): string {
-	let nameSplitDots: string[] = url.substr(url.lastIndexOf("/") + 1).split('.');
-	let nameSplitColons: string[] = nameSplitDots[1].split(':');
-
-	return `${nameSplitDots[0]}_${nameSplitColons[1]}.${nameSplitColons[0]}`;
+function makeFileNameFromUrl(url: URL): string {
+	let fileName: string = url.toString().match(/media\/(.*)\?/)?.[1] || 'downloadFile';
+	return `${fileName}_${url.searchParams.get('name')}.${url.searchParams.get('format')}`;
 }
